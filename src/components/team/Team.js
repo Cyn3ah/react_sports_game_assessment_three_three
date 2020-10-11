889951457907f99834f85f70b10fbe13afac57dd
Cyn3ah <theg1obe@hotmail.com>
2020-10-11 10:45:10 -0500

    "rearranging things"

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,58 +1,39 @@
 import React from 'react';
-// import Game from '../game/Game';
-// import Scoreboard from '../scoreboard/Scoreboard';
-// import Team from '../team/Team';
 
-// class Team extends React.Component {
-//     constructor(props) {
-//         super(props)
-
-//         this.state = {
-//             name: "",
-//             logo: {}
-//         }
 class Team extends React.Component {
     constructor(props) {
         super(props)
-        const
-        this.name = (this.state.Team.props.name),
-            this.logo = (this.props.logo),
-
-            this.visitingTeam = new Team(),
-            this.homeTeam = new Team(),
-            // (this.Game.props: className = "Team props")
-            // constructor(name, logo) {
-            render(); {
-
-            this.state = {
-                name: "this.props.name",
-                logo: "this.props.logo"
-            }
-            if (this.state.visitingTeam) {
-                // const Visitor = ((this.visitingTeam.name))
-                let Visitor = this.props.visitingTeam
-                let Home = this.props.homeTeam
-                return (
-                    <div className="Game">
-                        <h1>Welcome to {props.venue}</h1>
-                        <div className="stats">
-                            <Visitor />
-
-                            <div className="versus">
-                                <h1>VS</h1>
-                            </div>
-
-                            <Home
-                                name={props.homeTeam.name}
-                                logo={props.homeTeam.logoSrc}
-                            />
-
-                        </div>
-                    </div>
-                );
-            }
+        this.state = {
+            name: "this.props.name",
+            logo: "this.props.logo",
+            // Visitor: "this.props.visitingTeam",
+            // Home: "this.props.homeTeam",
         }
+        const Visitor = this.visitingTeam;
+        const Home = this.homeTeam;
+
+        // if (this.state.visitingTeam)
+
+        // render() {
+        return (
+            <div className="Game">
+                <h1>Welcome to {(props.venue)}</h1>
+                <div className="Team">
+                    <h1>{(Visitor)}</h1>
+                </div>
+                <div className="Versus">
+                    <h1>VS</h1>
+                </div>
+                <div className="Team">
+                    <h1>{(Home)}</h1>
+                </div>
+            </div>
+        )
     }
 }
+// }
+
+
 
 export default Team
+
